fdeb04093f724aadad0c30f83fa210efe7e292eb
David Caicedo <davidfcc7@gmail.com>
2021-05-09 19:47:39 -0500

    api rest, react, json, bottstrap y axios

--- ejercicio-crud-api/src/App.js
+++ ejercicio-crud-api/src/App.js
@@ -1,73 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 import axios from 'axios';
-import "bootstrap/dist/css/bootstrap.min.css";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
-import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 const url = "https://crudcrud.com/api/d195aa0edbc346b6a0dd99815dd0d959";
 
-const mascotas = [
-  {id: 1, nombre: 'Toby', edad: 2, especie: 'Gato'},
-  {id: 2, nombre: 'Hulk', edad: 5, especie: 'Perro'},
-  {id: 3, nombre: 'Sasha', edad: 2, especie: 'Uron'},
-  {id: 4, nombre: 'victor', edad: 1, especie: 'Pajaro'},
-  {id: 5, nombre: 'Camilo', edad: 3, especie: 'Hamster'}
-]
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    mascotas: mascotas
+    data: [],
+    insertar: false,
+    form: {
+      id: '',
+      nombre: '',
+      edad: '',
+      especie: ''
+    }
   }
-  peticionGet = () =>{
-    axios.get(url).then(Response=>{
-      console.log(Response.mascotas);
+  peticionGet = () => {
+    axios.get(url).then(Response => {
+      this.setState({ data: Response.data })
+    }).catch(error => {
+      console.log(error.message);
     })
   }
-  componentDidMount(){
+  peticionPost = async () => {
+    delete this.state.form.id;
+    await axios.post(url,this.state.form).then(Response => {
+      this.insertar();
+      this.peticionGet();
+    }).catch(error => {
+      console.log(error.message);
+    })
+  }
+  insertar = () => {
+    this.setState({ insertar: !this.state.insertar });
+  }
+  handleChange = async e => {
+    e.persist();
+    await this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+    console.log(this.state.form);
+  }
+  componentDidMount() {
     this.peticionGet();
   }
-  render(){
-    return(
-      <>
-      <div>
-        <section>
-          <button>Ingresar nueva mascota</button>
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nombre</th>
-                <th>Edad</th>
-                <th>Especie</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                this.state.mascotas.map((elemento) => (
-                    <tr>
-                      <td>{elemento.id}</td>
-                      <td>{elemento.nombre}</td>
-                      <td>{elemento.edad}</td>
-                      <td>{elemento.especie}</td>
-                      <td>
-                        <button>Editar</button>
-                        <button>Eliminar</button>
-                      </td>
-                    </tr>
-                  )
-                )
-              }
-            </tbody>
-          </table>
-        </section>
-      </div>      
-      </>
-    )
+  render() {
+    const { form } = this.state;
+    return (
+      <div className="App">
+        <button className="btn btn-success" onClick={()=>this.insertar()}>Agregar Mascota</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nombre</th>
+              <th>Edad</th>
+              <th>Especie</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(mascota => {
+              return (
+                <tr>
+                  <td>{mascota.id}</td>
+                  <td>{mascota.nombre}</td>
+                  <td>{mascota.edad}</td>
+                  <td>{mascota.especie}</td>
+                  <td>
+                    <button className="btn btn-primary" onClick={() => this.peticionPost()}><FontAwesomeIcon icon={faEdit} /></button>{"   "}
+                    <button className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt} /></button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <Modal isOpen={this.state.insertar}>
+          <ModalHeader style={{ display: 'block' }}>
+            <span style={{ float: 'right' }}>X</span>
+          </ModalHeader>
+          <ModalBody>
+            <div className="form-group">
+              <label htmlFor="id">Id: </label>
+              <input className="form-control" type="text" name="id" id="id" readOnly onChange={this.handleChange} value={this.state.data.length + 1} /><br></br>
+              <label htmlFor="nombre">Nombre: </label>
+              <input className="form-control" type="text" name="nombre" id="nombre" onChange={this.handleChange} value={form.nombre} /><br></br>
+              <label htmlFor="edad">Edad: </label>
+              <input className="form-control" type="text" name="edad" id="edad" onChange={this.handleChange} value={form.edad} /><br></br>
+              <label htmlFor="especie">Especie: </label>
+              <input className="form-control" type="text" name="especie" id="especie" onChange={this.handleChange} value={form.especie} /><br></br><br></br>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-success" onClick={()=>this.peticionPost()}>insertar</button>
+            <button className="btn btn-danger" onClick={()=>this.insertar()}>Cancelar</button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
   }
 }
-
 export default App;
